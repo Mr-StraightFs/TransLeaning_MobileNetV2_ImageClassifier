fdfdc1bf1ea7973927d6fb11307e189fd5fcc130
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-18 13:30:47 +0100

    a Model to modify the classifier task so that it's able to recognize alpacas

--- TransLearning.py
+++ TransLearning.py
@@ -80,4 +80,102 @@ base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
                                                include_top=True,
                                                weights='imagenet')
 
+#Print the model summary below to see all the model's layers, the shapes of their outputs,
+# and the total number of parameters, trainable and non-trainable
+base_model.summary()
 
+# Single out the last two layers , namely global_average_pooling2d and predictions
+# They are the layers resposible for the classification and hence the ones we want to tune up for the purpose of this Project
+nb_layers = len(base_model.layers)
+print(base_model.layers[nb_layers - 2].name)
+print(base_model.layers[nb_layers - 1].name)
+
+# running the first batch through the MonilenetV2 , batch size = 32
+image_batch, label_batch = next(iter(train_dataset))
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+#Shows the different label probabilities in one tensor
+label_batch
+
+# Layer Freezing with the Functional API
+# a Model to modify the classifier task so that it's able to recognize alpacas
+def alpaca_model(image_shape=IMG_SIZE, data_augmentation=data_augmenter()):
+    ''' Define a tf.keras model for binary classification out of the MobileNetV2 model
+    Arguments:
+        image_shape -- Image width and height
+        data_augmentation -- data augmentation function
+    Returns:
+    Returns:
+        tf.keras.model
+    '''
+
+    input_shape = image_shape + (3,)
+
+    base_model = tf.keras.applications.MobileNetV2(input_shape=input_shape,
+                                                   include_top=False,  # Delete the top layer (the classification layer)
+                                                   weights='imagenet')  # From imageNet
+
+    # freeze the base model by making it non trainable
+    base_model.trainable = False
+
+    # create the input layer (Same as the imageNetv2 input size)
+    inputs = tf.keras.Input(shape=input_shape)
+
+    # apply data augmentation to the inputs
+    x = data_augmentation(inputs)
+
+    # data preprocessing using the same weights the model was trained on
+    x = preprocess_input(x)
+
+    # set training to False to avoid keeping track of statistics in the batch norm layer
+    x = base_model(x, training=False)
+
+    # add the new Binary classification layers
+    # global avg pooling to summarize the info in each channel
+    x = tfl.GlobalAveragePooling2D()(x)
+    # Dropout with probability of 0.2 to avoid overfitting
+    x = tfl.Dropout(0.2)(x)
+
+    # Prediction layer with one neuron (as a binary classifier only needs one)
+    outputs = tfl.Dense(1, activation='linear')(x)
+
+    model = tf.keras.Model(inputs, outputs)
+
+    return model
+
+
+model2 = alpaca_model(IMG_SIZE, data_augmentation)
+
+base_learning_rate = 0.001
+model2.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+initial_epochs = 5
+history = model2.fit(train_dataset, validation_data=validation_dataset, epochs=initial_epochs)
+
+# extract and Prep data for vizualisation
+acc = [0.] + history.history['accuracy']
+val_acc = [0.] + history.history['val_accuracy'
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Plot the training and validation accuracy
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
